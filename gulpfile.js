b37ba8ba6cebbd8ba84a3c91a81a4ead9119fdd8
Алина <alina.kala4ik@gmail.com>
2019-12-10 11:46:16 +0300

    Убирает запуск ссервера по команде build

--- gulpfile.js
+++ gulpfile.js
@@ -100,5 +100,5 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html", "server"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
